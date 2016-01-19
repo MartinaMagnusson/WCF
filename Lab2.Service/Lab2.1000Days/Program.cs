606b948a473eacd2379972ccc5b376edb6c448cc
Martina Magnusson <magnusson.martina@hotmail.com>
2016-01-19 10:47:13 +0100

    Skapat en ny service för att beräkna hur många dagar det är till nästa 1000tal

---
+++ Lab2.Service/Lab2.1000Days/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Lab2._1000Days
+{
+    [ServiceContract(Namespace = "http://Lab2._1000Days")]
+    public interface INext1000Days
+    {
+        [OperationContract]
+        string CalculateNext1000Days(DateTime birthday);
+    }
+    public class Next1000Days : INext1000Days
+    {
+        public string CalculateNext1000Days(DateTime birthday)
+        {
+            var days = (DateTime.Now - birthday);
+            return "";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri address = new Uri("http://localhost:8080/Lab2._1000Days");
+            ServiceHost serviceHost = new ServiceHost(typeof(Next1000Days), address);
+            try
+            {
+                serviceHost.AddServiceEndpoint(typeof(INext1000Days),
+                    new WSHttpBinding(),
+                    "Next1000Days");
+                ServiceMetadataBehavior smBehavior = new ServiceMetadataBehavior();
+                smBehavior.HttpGetEnabled = true;
+                serviceHost.Description.Behaviors.Add(smBehavior);
+
+                serviceHost.Open();
+                Console.WriteLine("Tjänsten är öppen!");
+                Console.WriteLine("Tryck enter för att avsluta");
+                Console.ReadLine();
+            }
+            catch (CommunicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                throw;
+            }
+            finally
+            {
+                serviceHost.Close();
+            }
+        }
+    }
+}
